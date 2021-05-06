4e9628ed47929bf9137672d8f7354f5dfbd362e3
Willy Hille <willy.hille@intenta.de>
2021-05-06 15:52:58 +0200

    manually cd into a subdir for compiling
    this allows minted to find the _minted_main folder while maintaining a clean working tree

--- compile.sh
+++ compile.sh
@@ -1,9 +1,14 @@
-if [ ! "$CLEAN_BUILD" == "" ]; then
-  rm -r /latex/latex.out
+# ln -s /latex/_minted-main /latex/latex.out/_minted-main
+if [ ! "$CLEAN_BUILD" == "" ] && [ -d "/latex/$BUILD_DIRECTORY_RELATIVE" ]; then
+  rm -r /latex/$BUILD_DIRECTORY_RELATIVE
 fi
-python3 /latexrun.py --latex-args=--shell-escape --bibtex-cmd biber $WARNINGS $TARGET
+mkdir -p /latex/$BUILD_DIRECTORY_RELATIVE
+cp -r /latex/* /latex/$BUILD_DIRECTORY_RELATIVE/
+cd /latex/$BUILD_DIRECTORY_RELATIVE
+python3 /latexrun.py --latex-args=--shell-escape --bibtex-cmd biber -O . $WARNINGS $TARGET && \
+cp /latex/$BUILD_DIRECTORY_RELATIVE/main.pdf /latex/main.pdf && \
 if [ ! "$DELETE_TEMP" == "" ]; then
-  rm -r /latex/latex.out
+  rm -r /latex/$BUILD_DIRECTORY_RELATIVE
 fi
 
 #pdflatex --shell-escape "$TARGET"
