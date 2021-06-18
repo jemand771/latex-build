85fe3854fcded1eb1270c1aefaae292c9d50e58d
jemand771 <jemand771@gmx.net>
2021-06-18 08:48:08 +0200

    run latexrun again regardless of first latexrun or pythontex result
    while this might be slower, it guarantees re-running pythontex code in case the previous build had errors

--- compile.sh
+++ compile.sh
@@ -7,9 +7,9 @@ mkdir -p "$BUILDDIR_FULL"
 cp -r -u "$BIND_PATH"/* "$BUILDDIR_FULL/"
 cd "$BUILDDIR_FULL"
 if [ "$DISABLE_PYTHONTEX" == "" ]; then
-python3 /latexrun.py --latex-args=--shell-escape --bibtex-cmd biber -O . $WARNINGS $TARGET > /dev/null && \
-pythontex --interpreter python:python3 main
-fi && \
+  python3 /latexrun.py --latex-args=--shell-escape --bibtex-cmd biber -O . $WARNINGS $TARGET > /dev/null
+  pythontex --interpreter python:python3 main
+fi
 python3 /latexrun.py --latex-args=--shell-escape --bibtex-cmd biber -O . $WARNINGS $TARGET && \
 cp -u "$BUILDDIR_FULL/main.pdf" "$BIND_PATH/main.pdf" && \
 if [ ! "$DELETE_TEMP" == "" ]; then
