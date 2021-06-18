995df41fded4597737fbdebd9ec6e272250446a4
jemand771 <jemand771@gmx.net>
2021-06-18 08:44:14 +0200

    use cp -u to only copy changed files
    this should slightly improve compile speed if the build folder contains large files (e.g. high-res images, files unrelated to the project, etc.)

--- compile.sh
+++ compile.sh
@@ -4,14 +4,14 @@ if [ ! "$CLEAN_BUILD" == "" ] && [ -d "$BUILDDIR_FULL" ]; then
   rm -r "$BUILDDIR_FULL"
 fi
 mkdir -p "$BUILDDIR_FULL"
-cp -r "$BIND_PATH"/* "$BUILDDIR_FULL/"
+cp -r -u "$BIND_PATH"/* "$BUILDDIR_FULL/"
 cd "$BUILDDIR_FULL"
 if [ "$DISABLE_PYTHONTEX" == "" ]; then
 python3 /latexrun.py --latex-args=--shell-escape --bibtex-cmd biber -O . $WARNINGS $TARGET > /dev/null && \
 pythontex --interpreter python:python3 main
 fi && \
 python3 /latexrun.py --latex-args=--shell-escape --bibtex-cmd biber -O . $WARNINGS $TARGET && \
-cp "$BUILDDIR_FULL/main.pdf" "$BIND_PATH/main.pdf" && \
+cp -u "$BUILDDIR_FULL/main.pdf" "$BIND_PATH/main.pdf" && \
 if [ ! "$DELETE_TEMP" == "" ]; then
   rm -r "$BUILDDIR_FULL"
 fi
